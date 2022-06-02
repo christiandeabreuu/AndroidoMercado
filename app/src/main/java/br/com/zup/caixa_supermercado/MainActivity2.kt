9668cf8588f734f96e952c7b5a3a907d18344dde
FepaZup <felipe.dearaujo@zup.com.br>
2022-06-02 15:33:16 -0300

    terceiro commit

--- app/src/main/java/br/com/zup/caixa_supermercado/MainActivity2.kt
+++ app/src/main/java/br/com/zup/caixa_supermercado/MainActivity2.kt
@@ -3,7 +3,9 @@ package br.com.zup.caixa_supermercado
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 import br.com.zup.caixa_supermercado.databinding.ActivityMain2Binding
 import br.com.zup.caixa_supermercado.databinding.ActivityMainBinding
 import br.com.zup.caixa_supermercado.model.Produto
@@ -14,19 +16,16 @@ class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
-        var produto  =  intent.getParcelableExtra<Produto>("Produto")
-        val binding : ActivityMain2Binding =  ActivityMain2Binding.inflate(layoutInflater)
-        produto?.let {
-            val total = it.getQtd() * it.getValor()
+        var produto = intent.getParcelableExtra<Produto>("Produto")
+        val binding: ActivityMain2Binding = ActivityMain2Binding.inflate(layoutInflater)
+        if (produto != null) {
+            val total = produto.getQtd() * produto.getValor()
             val textofinal = getString(R.string.descricao_soma_carrinho).plus(total)
             binding.tvValorTotal.text = textofinal
+            //Toast.makeText(this, textofinal, Toast.LENGTH_LONG).show()
         }
     }
-
-    override fun onStart() {
-
-        super.onStart()
-
-    }
 }
+
+
 
